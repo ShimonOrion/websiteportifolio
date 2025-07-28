756fa89362a13225e3a3331c7086f839474725ba
Daniel Bacelar <dev007bacelar@gmail.com>
2025-07-28 16:09:26 -0300

    Finalizando a página home

--- html+css+js/js/main.js
+++ html+css+js/js/main.js
@@ -1,6 +1,16 @@
+/*  Declarando Variáveis   */
+
 var btnContact = document.querySelector(".db-btn-contact");
 var contactInfo = document.querySelector(".db-contact-info");
 
+/*  Page Preloader  */
+
+window.addEventListener("load", function () {
+  var pagePreloader = document.querySelector(".db-preloader");
+  pagePreloader.classList.add("db-fade-out");
+});
+
+/*  Função para exibir/ocultar informações de contato  */
 btnContact.addEventListener("click", function () {
   var boxContact = document.querySelector(".db-contact-info");
 
